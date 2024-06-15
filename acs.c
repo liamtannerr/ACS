366909cbf62c748605a5e89642e5fc730f075546
liamtanner <liam.tanner@brentwood.ca>
2024-06-15 14:18:17 -0700

    customers are in business and economy arrays

--- acs.c
+++ acs.c
@@ -31,28 +31,41 @@ int main(){
 		return 0;
 	}
 
-	int num_customers = atoi(fgets(buf ,2,(customers_file)));
-
+	int num_customers = atoi(fgets(buf, sizeof(buf), (customers_file)));
 	int customer_id;
 	int customer_class;
 	int customer_arrival_time;
 	int customer_service_time;
 
-	customer* customers = malloc(num_customers * sizeof(customer));
-
+	customer* business_customers = malloc(num_customers * sizeof(customer));
+	customer* economy_customers = malloc(num_customers * sizeof(customer));
+	char line [256];
+	int num_business = 0;
+	int num_economy = 0;
 	
-	
-	while (!feof(customers_file)) {
+	for(int i = 0; i < num_customers; i++){
+		if(fgets(line, sizeof(line), customers_file) != NULL){
+			sscanf(line, "%d:%d,%d,%d", &customer_id, &customer_class, &customer_arrival_time, &customer_service_time);
+			if(customer_class == 1){
+				business_customers[num_business].id = customer_id;
+				business_customers[num_business].class = customer_class;
+				business_customers[num_business].arrival_time = customer_arrival_time;
+				business_customers[num_business].service_time = customer_service_time;
+				num_business++;				
+			} else {
+				economy_customers[num_economy].id = customer_id;
+				economy_customers[num_economy].class = customer_class;
+				economy_customers[num_economy].arrival_time = customer_arrival_time;
+				economy_customers[num_economy].service_time = customer_service_time;
+				num_economy++;
+			}
 
-
-    
-		scanf(line, "", &customer_id, &customer_class, &customer_arrival_time, &customer_service_time);
- 	
-	        
+		}
 	}
-	
+
  
 	fclose(customers_file);
- 
+	free(business_customers);
+	free(economy_customers);
     return 0;
 }
