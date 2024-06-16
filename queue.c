9a4c7d6b3a2ee683c140a57f0a655bafe6638a5f
liamtanner <liam.tanner@brentwood.ca>
2024-06-15 18:40:19 -0700

    enqueue and create queue done

--- queue.c
+++ queue.c
@@ -4,12 +4,26 @@
 
 // Function to create a queue
 Queue* createQueue() {
-
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q -> front = NULL;
+    q -> rear = NULL;
+    q -> size = 0;
+    return q;
 }
 
 // Function to add a customer to the queue
 void enqueue(Queue* q, customer c) {
-
+    Node* n = (Node*)malloc(sizeof(Node));
+    n -> cust = c;
+    n -> next = NULL;
+    if(isEmpty(q)){
+        q -> front = n;
+        q -> rear = n;
+    } else {
+        q -> rear -> next = n;
+        q -> rear = n;
+    }
+    q -> size++;
 }
 
 // Function to remove a customer from the queue
